1e7d7d6bfeb5d82eb48fdcb2089ea69d33752a4f
Vincent Tencé <vtence@gmail.com>
2014-11-19 20:15:16 -0500

    Streams are fun but this is obviously much simpler

--- src/main/java/com/vtence/jyose/fire/Terrain.java
+++ src/main/java/com/vtence/jyose/fire/Terrain.java
@@ -1,13 +1,11 @@
 package com.vtence.jyose.fire;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.IntStream.range;
 
 public class Terrain {
     private final List<List<Integer>> tiles;
@@ -40,30 +38,12 @@ public class Terrain {
     }
 
     public Stream<Pos> findAll(int what) {
-        return rowsContaining(what).flatMap(row -> locateAll(what).apply(row));
-    }
-
-    private Stream<Integer> rowsContaining(int what) {
-        return allRows().filter(rowContains(what));
-    }
-
-    private Stream<Integer> allRows() {
-        return range(0, tiles.size()).boxed();
-    }
-
-    private Predicate<Integer> rowContains(int what) {
-        return row -> tiles.get(row).contains(what);
-    }
-
-    private Function<Integer, Stream<Pos>> locateAll(int what) {
-        return row -> allColsOf(row).filter(colContains(what).apply(row)).map((col) -> Pos.at(row, col));
-    }
-
-    private Stream<Integer> allColsOf(int row) {
-        return range(0, tiles.get(row).size()).boxed();
-    }
-
-    private Function<Integer, Predicate<Integer>> colContains(int what) {
-        return (row) -> col -> tiles.get(row).get(col) == what;
+        List<Pos> all = new ArrayList<>();
+        for (int row = 0; row < tiles.size(); row++) {
+            for (int col = 0; col < tiles.get(row).size(); col++) {
+                if (tiles.get(row).get(col) == what) all.add(Pos.at(row, col));
+            }
+        }
+        return all.stream();
     }
 }
