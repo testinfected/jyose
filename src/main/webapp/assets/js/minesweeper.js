1e6adbffe4ec95ef7516d40a503f9250f4f405c8
Vincent Tencé <vtence@gmail.com>
2014-12-11 17:28:48 -0500

    Extract a cell class from our minesweeper code, and bind click events from the board. Now to push more behavior to this newly born class

--- src/main/webapp/assets/js/minesweeper.js
+++ src/main/webapp/assets/js/minesweeper.js
@@ -17,107 +17,130 @@ minesweeper.Board = function (grid) {
         return row >= 0 && row < grid.length && col >= 0 && col < grid[row].length;
     }
 
-    function cell(row, col) {
-        function id(row, col) {
-            return 'cell-' + (row + 1) + 'x' + (col + 1);
+    function id(row, col) {
+        return 'cell-' + (row + 1) + 'x' + (col + 1);
+    }
+
+    function neighbors(row, col) {
+        var around = [
+            [-1, -1], [-1, 0], [-1, 1],
+            [0, -1],           [0, 1],
+            [1, -1],  [1, 0],  [1, 1]
+        ];
+
+        return around.map(function (pos) {
+            return {row: row + pos[0], col: col + pos[1]}
+        });
+    }
+
+    function legalNeighbors(row, col) {
+        return neighbors(row, col).filter(function (pos) { return legal(pos.row, pos.col); });
+    }
+
+    function unknownNeighbors(row, col) {
+        return legalNeighbors(row, col).filter(function (pos) { return !revealed(pos.row, pos.col); });
+    }
+
+    function bombsAround(row, col) {
+        var bombs = 0;
+        legalNeighbors(row, col).forEach(function (pos) {
+            if (bombAt(pos.row, pos.col)) bombs += 1;
+        });
+        return bombs;
+    }
+
+    function $(row, col) {
+        return document.getElementById(id(row, col));
+    }
+
+    function markSafe(row, col) {
+        var cell = $(row, col);
+        cell.className = 'safe';
+
+        var bombs = bombsAround(row, col);
+        if (bombs != 0) {
+            cell.textContent = bombs.toString();
+            cell.className += ' safe-' + bombs
+        }
+    }
+
+    function markLost(row, col) {
+        $(row, col).className = 'lost';
+    }
+
+    function revealed(row, col) {
+        var classes = $(row, col).className;
+        return ~classes.indexOf('lost') || ~classes.indexOf('safe');
+    }
+
+    function reveal(row, col) {
+        if (flagged(row, col)) return;
+
+        if (bombAt(row, col)) {
+            markLost(row, col);
+            return;
         }
 
-        function neighbors(row, col) {
-            var around = [
-                [-1, -1], [-1, 0], [-1, 1],
-                [0, -1],           [0, 1],
-                [1, -1],  [1, 0],  [1, 1]
-            ];
+        markSafe(row, col);
 
-            return around.map(function (pos) {
-                return {row: row + pos[0], col: col + pos[1]}
+        if (bombsAround(row, col) == 0) {
+            unknownNeighbors(row, col).forEach(function (pos) {
+                reveal(pos.row, pos.col);
             });
         }
+    }
 
-        function legalNeighbors(row, col) {
-            return neighbors(row, col).filter(function (pos) { return legal(pos.row, pos.col); });
-        }
+    function flagged(row, col) {
+        return ~$(row, col).className.indexOf('suspect');
+    }
 
-        function unknownNeighbors(row, col) {
-            return legalNeighbors(row, col).filter(function (pos) { return !revealed(pos.row, pos.col); });
-        }
+    function toggleFlag(row, col) {
+        if (revealed(row, col)) return;
+        $(row, col).className = flagged(row, col) ? '' : 'suspect';
+    }
 
-        function bombsAround(row, col) {
-            var bombs = 0;
-            legalNeighbors(row, col).forEach(function (pos) {
-                if (bombAt(pos.row, pos.col)) bombs += 1;
+    function Cell(row, col) {
+        this.row = row;
+        this.col = col;
+
+        this.onclick = function(handler) {
+            this.elem.addEventListener('click', function(event) {
+                if (event.which != RIGHT_BUTTON && !minesweeper.suspectMode) handler(event);
             });
-            return bombs;
-        }
+        };
 
-        function $(row, col) {
-            return document.getElementById(id(row, col));
-        }
+        this.onrightclick = function(handler) {
+            this.elem.addEventListener('click', function(event) {
+                if (event.which == RIGHT_BUTTON || minesweeper.suspectMode) handler(event);
+            });
+            this.elem.addEventListener('contextmenu', function (event) {
+                event.preventDefault();
+                handler(event);
+            });
+        };
 
-        function markSafe(row, col) {
-            var cell = $(row, col);
-            cell.className = 'safe';
-
-            var bombs = bombsAround(row, col);
-            if (bombs != 0) {
-                cell.textContent = bombs.toString();
-                cell.className += ' safe-' + bombs
-            }
-        }
-
-        function markLost(row, col) {
-            $(row, col).className = 'lost';
-        }
-
-        function revealed(row, col) {
-            var classes = $(row, col).className;
-            return ~classes.indexOf('lost') || ~classes.indexOf('safe');
-        }
-
-        function reveal(row, col) {
-            if (flagged(row, col)) return;
-
-            if (bombAt(row, col)) {
-                markLost(row, col);
-                return;
-            }
-
-            markSafe(row, col);
-
-            if (bombsAround(row, col) == 0) {
-                unknownNeighbors(row, col).forEach(function (pos) {
-                    reveal(pos.row, pos.col);
-                });
-            }
-        }
-
-        function flagged(row, col) {
-            return ~$(row, col).className.indexOf('suspect');
-        }
-
-        function toggleFlag(row, col) {
-            if (revealed(row, col)) return;
-            $(row, col).className = flagged(row, col) ? '' : 'suspect';
-        }
-
-        var cell = document.createElement('td');
-        cell.id = id(row, col);
-        cell.addEventListener('click', function (event) {
-            if (event.which == RIGHT_BUTTON || minesweeper.suspectMode) {
-                toggleFlag(row, col);
-            } else {
-                reveal(row, col);
-            }
-        });
-        cell.addEventListener('contextmenu', function (event) {
-            event.preventDefault();
-            toggleFlag(row, col);
-        });
-        return cell;
+        this.render = function() {
+            this.elem = document.createElement('td');
+            this.elem.id = id(this.row, this.col);
+            return this.elem;
+        };
     }
 
     this.render = function (on) {
         on.innerHTML = '';
+
+        function cell(row, col) {
+            var cell = new Cell(row, col);
+            var td = cell.render();
+            cell.onclick(function (event) {
+                reveal(row, col);
+            });
+            cell.onrightclick(function (event) {
+                toggleFlag(row, col);
+            });
+            return td;
+        }
+
         function line(row) {
             var tr = document.createElement('tr');
             for (var col = 0; col < grid[row].length; col++) {
